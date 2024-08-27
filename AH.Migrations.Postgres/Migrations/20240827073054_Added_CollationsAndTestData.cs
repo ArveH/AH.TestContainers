11f5cae017fa0c81ad1d0d0b13a617e2bd4e6b94
Arve Hansen <arveh@outlook.com>
2024-08-27 09:31:31 +0200

    Added collations and test data New EF migration for this.

---
+++ AH.Migrations.Postgres/Migrations/20240827073054_Added_CollationsAndTestData.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AH.Migrations.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_CollationsAndTestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:CollationDefinition:en_ci_ai", "en-u-ks-level1,en-u-ks-level1,icu,False")
+                .Annotation("Npgsql:CollationDefinition:en_ci_as", "en-u-ks-level2,en-u-ks-level2,icu,False");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "People",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                collation: "en_ci_ai",
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "People",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                collation: "en_ci_ai",
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.InsertData(
+                table: "People",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1L, "John", "Doe" },
+                    { 2L, "Jane", "Doe" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:CollationDefinition:en_ci_ai", "en-u-ks-level1,en-u-ks-level1,icu,False")
+                .OldAnnotation("Npgsql:CollationDefinition:en_ci_as", "en-u-ks-level2,en-u-ks-level2,icu,False");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "People",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255,
+                oldCollation: "en_ci_ai");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "People",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255,
+                oldCollation: "en_ci_ai");
+        }
+    }
+}
--- AH.Model/AhDbContext.cs
+++ AH.Model/AhDbContext.cs
@@ -5,10 +5,36 @@ namespace AH.Model;
 
 public class AhDbContext : DbContext
 {
+    private const string CIAICollation = "en_ci_ai";
+    private const string CIASCollation = "en_ci_as";
+
     public AhDbContext(DbContextOptions<AhDbContext> options)
         : base(options)
-    { }
+    {
+    }
 
     public DbSet<Person> People => Set<Person>();
 
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<string>().UseCollation(CIAICollation);
+        base.ConfigureConventions(configurationBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasCollation(CIAICollation,
+            provider: "icu",
+            locale: "en-u-ks-level1",
+            deterministic: false);
+        modelBuilder.HasCollation(CIASCollation,
+            provider: "icu",
+            locale: "en-u-ks-level2",
+            deterministic: false);
+
+        modelBuilder.Entity<Person>().HasData(
+            new Person { Id = 1, FirstName = "John", LastName = "Doe" },
+            new Person { Id = 2, FirstName = "Jane", LastName = "Doe" }
+        );
+    }
 }
