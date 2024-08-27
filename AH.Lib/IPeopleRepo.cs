a67bf58d0d61e31de3d74fb41b0d2ed9c51969c9
Arve Hansen <arveh@outlook.com>
2024-08-27 09:52:04 +0200

    Added Lib

---
+++ AH.Lib/IPeopleRepo.cs
@@ -0,0 +1,9 @@
+﻿using AH.Database.Entities;
+
+namespace AH.Lib;
+
+public interface IPeopleRepo
+{
+    Task<IEnumerable<Person>> GetPeopleAsync();
+    void Dispose();
+}
---
+++ AH.Lib/PeopleRepo.cs
@@ -0,0 +1,32 @@
+﻿using AH.Database.Entities;
+using AH.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AH.Lib
+{
+    public class PeopleRepo(AhDbContext context) : IDisposable, IPeopleRepo
+    {
+        public async Task<IEnumerable<Person>> GetPeopleAsync()
+        {
+            return await context.People.ToListAsync();
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                context.Dispose();
+            }
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
